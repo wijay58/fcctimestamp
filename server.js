1eb3faf1d2709ab08e80b3563bb30312e01ea86f
wijay58 <supun.wijemanna58@gmail.com>
2021-11-07 17:35:19 +0530

    dxdsdsd

--- server.js
+++ server.js
@@ -17,11 +17,31 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+app.get("/api",function(req,res){
+  let datenow = new Date();
+  res.json({"unix": datenow.getTime(),"utc": datenow.toUTCString()});
+})
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+  if(date == 'Invalid Date') return res.json({error: "Invalid Date"});
+  try {
+    let unix = date.getTime();
+    let utc = date.toUTCString();
+    if(isNaN(unix)) {
+      date = new Date(parseInt(req.params.date));
+      unix = date.getTime();
+      utc = date.toUTCString();
+    }
+    res.json({"unix": unix,"utc": utc});
+  } catch (error) {
+    res.json({error: "Invalid Date"});
+  }
 
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({unix: 'hello API',});
 });
 
 
